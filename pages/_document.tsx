16ee404846760f31ba1045add5be5216ab66c0eb
Stefan Mermans <stefanmermans99@gmail.com>
2022-09-15 22:52:47 +0200

    lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:100,300,400&display=swap"
